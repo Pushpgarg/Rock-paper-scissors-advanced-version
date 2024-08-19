d4dd812c76f353d4c36fd547a55ae6718ce248d5
Pushpgarg <Pushpgarg9765@gmail.com>
2024-08-19 22:52:39 +0530

    replaced the cmd to download stopwords each times and directly initiatted the stopwords as a list

--- TextProcessing.py
+++ TextProcessing.py
@@ -4,14 +4,17 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 # Download necessary NLTK data files
-nltk.download('stopwords')
+# nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
 # Initialize lemmatizer and stopwords
 lemmatizer = WordNetLemmatizer()
-all_stopwords = stopwords.words('english')
+# all_stopwords = stopwords.words('english')
+all_stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
+print(type(all_stopwords))
+print(all_stopwords)
 # Specify words to remove from stopwords
 words_to_remove_from_stopwords = [
     'not','in',"can't",'no','nor','a','b','c','d','with','but','don', "don't",'ain',
